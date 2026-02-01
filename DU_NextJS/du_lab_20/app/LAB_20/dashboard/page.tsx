8ec76992f867d36d7d1bd0ff968bb4ea978fe5db
BhaviikCode-5202 <er.bhavik5202@gmail.com>
2026-02-01 23:01:58 +0530

    Initial commit

---
+++ DU_NextJS/du_lab_20/app/LAB_20/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function DashboardPage() {
+  return (
+    <>
+      <h1>Hello From Dashboard Page</h1>
+    </>
+  );
+}
---
+++ DU_NextJS/du_lab_20/app/LAB_20/page.tsx
@@ -0,0 +1,13 @@
+export default function Lab20Page() {
+  return (
+  <>
+    <div className="space-y-6">
+        <div>
+          <h2 className="text-2xl font-bold text-gray-800 mb-4">
+            Welcome to Lab 20
+          </h2>
+        </div>
+    </div>
+  </>
+  );
+}
